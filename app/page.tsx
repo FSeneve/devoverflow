cbe08cfa6f3ac51adb1698bee048c9bc099918d6
Francis Seneve <francisseneve@gmail.com>
2024-12-21 11:14:53 +0100

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-center text-3xl text-light-500">Hello World</h1>
+      <h1 className="text-blue-500 text-center font-inter">Hello World</h1>
+        <h1 className="font-space-grotesk text-center text-blue-500">Hello World 2</h1>
     </div>
   );
 }
